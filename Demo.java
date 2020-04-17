ce0627df96f06b8ff58409adf28cee9381c045b3
phuonglemy99 <phuonglemy1199@gmail.com>
2020-04-17 08:40:03 +0700

    Second edition - stringBuilder to String

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo {
     public static void main(String[] args){
         StringBuilder stringBuilder = new StringBuilder("Hello LAMP, Hi LAMP, Bonjour LAMP");
         System.out.println("String :- " + stringBuilder);
-
+    
         String str = "LAMP"; // String you want to search
         int fromIndex_1 = 17;  // Index 17 is M in second LAMP
         int fromIndex_2 = 12;  // Index 12 is i in Hi
